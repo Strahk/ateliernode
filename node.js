118254956ba94382fd9a913d916cde9c2e56ff9b
Ismael Repir <repirismael6@gmail.com>
2019-10-02 16:55:27 +0200

    ajout atelier 2

--- node.js
+++ node.js
@@ -5,3 +5,13 @@ console.log(chalk.blue('Julien'))
 console.log(chalk.white('Inès'))
 console.log(chalk.red('Marion'))
 console.log(chalk.yellow('Lucie'))
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200);
+    response.end('Hello World');
+});
+
+server.listen(3000);
+console.log('Adresse du serveur: http://localhost:3000');
