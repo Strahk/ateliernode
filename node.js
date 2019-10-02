c5cde24209e8e6652750bfcc4146d4de7daf2830
Ismael Repir <repirismael6@gmail.com>
2019-10-02 16:29:05 +0200

    atelier_node

---
+++ node.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Julien')); 
+log(chalk.white('Inès'));
+log(chalk.pink('Marion'));
+log(chalk.yellow('Lucie'));
